c2e025c870f250b9261e1a3eb68963b4a30c267d
VatsalP26 <vatsalprjpt26@gmail.com>
2025-04-06 22:42:52 -0400

    deployment configuration v2.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://comp3133-101414010-assignment1.onrender.com', 
+    apiUrl: 'https://comp3133-101414010-assignment1.onrender.com/graphql', 
   };
