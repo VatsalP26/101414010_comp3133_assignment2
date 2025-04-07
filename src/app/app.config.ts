65f7a6fb6bd8052ff2458ce89d4771ba74894e2c
VatsalP26 <vatsalprjpt26@gmail.com>
2025-04-06 23:45:02 -0400

    Updated readme file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,5 +2,5 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true })]
 };
